143b41ceed4f83629055457ad19276f4b33ef953
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-03-03 20:32:15 +0500

    Transfer code from html to script.js file

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
- 
+ 	
+//alert("hello");
+
+console.log("Liaqat");
+console.log(typeof 23);
+
+let firstName = "Waqas";
+
+console.log(firstName);
+
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "waqas";
+let PI = 3.1415;
+console.log(typeof PI);
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+console.log(typeof job2);
