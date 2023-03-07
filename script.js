57654c29e7aab6f29720cb9122a06cc0d1b0ff5a
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-03-07 19:32:34 +0500

    assignment and comparison operator testing

--- script.js
+++ script.js
@@ -77,10 +77,10 @@ console.log('123' < 57);
 console.log( 5 + 6 + '4' + 9 - 4 - 2);
 
 
+
+
+
 */
-
-
-
 // Basic Operators
 // Math operators
 const now = 2023;
@@ -97,3 +97,19 @@ console.log(firstName + ' ' + lastName);
 
 
 
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(agePerson > ageXyz); // >, <, >=, <=
+console.log(ageXyz >= 18);
+
+const isFullAge = ageXyz >= 18;
+
+console.log(now - 1991 > now - 2018);
