06cea1dd02d275dd1f875a6eaa42b6d2617b4d38
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-03-10 22:05:05 +0500

    Dot vs. Bracket Notation

--- script.js
+++ script.js
@@ -436,7 +436,7 @@ console.log(friends.includes(23));
 if (friends.includes('Steven')) {
   console.log('You have a friend called Steven');
 }
-*/
+
 // Objects Testing
 const jonasArray = [
   'Waqas  ',
@@ -455,7 +455,43 @@ const jonas = {
 };
 
 
+*/
 
+
+// Dot vs. Bracket Notation
+const jonas = {
+  firstName: 'Waqas',
+  lastName: 'Liaqat',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven']
+};
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+// console.log(jonas.'last' + nameKey)
+
+const interestedIn = prompt('What do you want to know about Waqas? Choose between firstName, lastName, age, job, and friends');
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+}
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschmedtman';
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
 
 
 
