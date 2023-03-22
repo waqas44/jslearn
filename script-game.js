3817aabeaf7cb9bffb6817b78cc8a6c2141f679b
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-03-22 17:12:17 +0500

    dom initial testing

---
+++ script-game.js
@@ -0,0 +1,12 @@
+'use strict';
+//let elm = document.querySelector('.message');
+let elm = document.querySelector('.message').textContent = 'New message here';
+
+document.querySelector('.number').textContent = 13;
+
+document.querySelector('.score').textContent = 20;
+
+console.log(document.querySelector('.guess').value);
+
+//console.log(elm)
+//console.log(document.querySelector('.message'));
