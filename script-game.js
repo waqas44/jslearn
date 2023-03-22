bdcde4e552276794af3e9c51f700be8a074a03cd
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-03-22 22:14:18 +0500

    dom initial testing 3

--- script-game.js
+++ script-game.js
@@ -1,19 +1,30 @@
 'use strict';
 //let elm = document.querySelector('.message');
-let elm = document.querySelector('.message').textContent = 'New message here';
+//let elm = document.querySelector('.message').textContent = 'New message here';
 
 document.querySelector('.number').textContent = 13;
 
 document.querySelector('.score').textContent = 20;
 
-document.querySelector('.guess').value = 15;
+// document.querySelector('.guess').value = 15;
+// document.querySelector('.check').addEventListener('click', function () {
+
+
+//     document.querySelector('.guess').value = 17;
+
+// });
+
 document.querySelector('.check').addEventListener('click', function () {
 
 
-    document.querySelector('.guess').value = 17;
+    let guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+    if (!guess) {
+        let num = document.querySelector('.message').textContent = 'No Number !';
+        console.log(num);
 
-})
-
+    }
+});
 
 
 
