16625a8130e4bc53746e969a6e59c7330ad4f6b6
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-03-28 16:32:27 +0500

    model box testing 1

---
+++ model.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+
