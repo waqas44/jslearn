f2036b7fa52c7a196a21837e68bbd16835a39e31
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-03-28 16:33:53 +0500

    model box close functionality added 2

--- model.js
+++ model.js
@@ -10,4 +10,14 @@ const openModal = function () {
     overlay.classList.remove('hidden');
 };
 
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
