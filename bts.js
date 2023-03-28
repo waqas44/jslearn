b54406848f357d02a56a83d3035acff2c9f5923c
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-03-28 19:34:24 +0500

    Variable scope testing and practice 2

--- bts.js
+++ bts.js
@@ -9,7 +9,23 @@ function calcAge(birthYear) {
         let output = `${firstName}, you are ${age}, born in ${birthYear}`;
         console.log(output);
 
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var millenial = true;
+            // Creating NEW variable with same name as outer scope's variable
+            const firstName = 'Waqas';
 
+            // Reasssigning outer scope's variable
+            output = 'NEW OUTPUT!';
+
+            const str = `Oh, and you're a SP, ${firstName}`;
+            console.log(str);
+
+            function add(a, b) {
+                return a + b;
+            }
+        }
+        // console.log(str);
+        console.log(millenial);
         // console.log(add(2, 3));
         console.log(output);
     }
