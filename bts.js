82ffec467e20ab58f2e39451698cbe4c40f1652b
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-03-28 19:32:38 +0500

    Variable scope testing and practice 1

---
+++ bts.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Scoping in Practice
+
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+
+    function printAge() {
+        let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+        console.log(output);
+
+
+        // console.log(add(2, 3));
+        console.log(output);
+    }
+    printAge();
+
+    return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+// console.log(age);
+// printAge();
