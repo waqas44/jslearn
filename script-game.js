4c9972649c0a5db56e9f1c4a5a76a2f4a5e8a31a
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-03-28 16:17:30 +0500

    dom random number game testing 5

--- script-game.js
+++ script-game.js
@@ -64,3 +64,16 @@ document.querySelector('.number').textContent = number1;
 //console.log(document.querySelector('.message'));
 
 //console.log(document.querySelector('.guess').value);
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    // document.querySelector('.message').textContent = 'Start guessing...';
+    displayMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+});
