931742e983f2cdd0c711385a241ee2478c5a7551
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-03-29 20:49:15 +0500

    Hoisting and TDZ in Practice test 4

--- bts.js
+++ bts.js
@@ -79,3 +79,11 @@ var numProducts = 10;
 function deleteShoppingCart() {
     console.log('All products deleted!');
 }
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
