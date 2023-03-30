f790a6d6368e1e73b7707eb95969702bd2883751
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-03-30 20:08:36 +0500

    The this Keyword in Practice and testing 2

--- bts.js
+++ bts.js
@@ -97,3 +97,9 @@ const calcAge = function (birthYear) {
     console.log(this);
 };
 calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+    console.log(2037 - birthYear);
+    console.log(this);
+};
+calcAgeArrow(1980);
