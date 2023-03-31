3e4e3d4992c848b93c93f4520eefcdf420394289
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-03-31 19:23:10 +0500

    Regular Functions vs. Arrow Functions Practice 4

--- bts.js
+++ bts.js
@@ -162,3 +162,12 @@ const jonas = {
 jonas.greet();
 jonas.calcAge();
 
+
+// arguments keyword
+const addExpr = function (a, b) {
+    console.log(arguments);
+    return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
