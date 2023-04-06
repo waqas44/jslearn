28ce7925db4ef074bc583d16b76108bdab459b96
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-04-06 15:39:03 +0500

    destructruing testing 2

--- destructring.js
+++ destructring.js
@@ -1,2 +1,24 @@
 'use strict';
-console.log('hi 2');
+// Destructuring Arrays
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+const arr2 = ['text1', 'text 2', 'text 3'];
+console.log(arr2);
+const [x1, y1, z1] = arr2;
+console.log(x1, y1, z1);
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto']
+}
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
