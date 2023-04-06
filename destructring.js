79092fb48002b96bce797f88aa7858a2b7554e4c
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-04-06 16:09:41 +0500

    destructruing testing 6

--- destructring.js
+++ destructring.js
@@ -18,7 +18,10 @@ const restaurant = {
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto']
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    }
 }
 let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
@@ -31,3 +34,8 @@ console.log(main, secondary);
 
 [main, secondary] = [secondary, main];
 console.log(main, secondary);
+console.log('old');
+
+// Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
