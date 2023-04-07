a4fa564efb87e43b562e9409f0ca5ea6cae33769
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-04-07 15:58:07 +0500

    The Spread Operator test 2

--- destructring.js
+++ destructring.js
@@ -55,5 +55,10 @@ console.log(i1, w1, j1);
 
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
-console.log(badNewArr);
+//console.log(badNewArr);
 
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
